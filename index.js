ea3100796a80a2ae7692be302bf241766b8aff1e
Matthew Caldwell <tehcaldwell@gmail.com>
2021-08-02 16:58:24 -0400

    Add frontend

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Last.fm library downloader');
-});
+app.use(express.static('public'));
 
 app.listen(port, () => {
     console.log(`Last.fm Library Downloader listening at http://localhost:${port}`);
