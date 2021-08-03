3d5cf3336c38e33a7b6c249bdced394fbbbd453f
Matthew Caldwell <tehcaldwell@gmail.com>
2021-08-02 21:56:56 -0400

    Load all pages and store to mongodb

--- index.js
+++ index.js
@@ -20,12 +20,37 @@ app.post('/load', async (req, res) => {
     let fromDb = true;
     if (!library) {
         fromDb = false;
+        // load first page
         const recentTracks = await lastfm.getRecentTracks(username);
-        library = {
-            username,
-            timestamp: new Date,
-            scrobbles: recentTracks.track || []
-        };
+        const totalPages = recentTracks['@attr'].totalPages;
+        // store first page
+        libraries.insertOne({ 
+            username, 
+            scrobbles: recentTracks.track, 
+            timestamp: new Date 
+        });
+        // load remaining pages
+        let scrobbles = [];
+        for (let page = 2; page <= totalPages; page++) {
+            await new Promise(res => setTimeout(res, 2000));
+            const resp = await lastfm.getRecentTracks(username, page);
+            scrobbles.push(...resp.track);
+            if (page % 5 === 0) {
+                console.log(`Page ${page} / ${totalPages}`);
+            }
+            if (page % 10 === 0) {
+                // update db with last 10 pages scrobbles
+                libraries.updateOne({ username }, {
+                    $push: { 
+                        scrobbles: {
+                            $each: scrobbles
+                        }
+                    }
+                });
+                scrobbles = [];
+            }
+        }
+        library = { username, scrobbles, timestamp: new Date };
         libraries.insertOne(library);
     }
 
